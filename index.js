7162323354ce288730c7553530e942c3c6b8a74c
gauravkmr065 <gauravkmr065>
2021-04-28 10:57:20 +0530

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const buttons = document.querySelectorAll('button');
+let screen = document.querySelector('#input');
+let screenValue='';
+for(b of buttons){
+    b.addEventListener('click',(ep)=>{
+        butn =ep.target.innerText;
+        // console.log(butn);
+        if(butn=='x'){
+           butn = '*';
+           screenValue +=butn;
+          screen.value = screenValue;
+        }else if(butn=='c'){
+            screenValue ="";
+            screen.value=screenValue;
+        } else if(butn=='=') {
+           screen.value= eval(screenValue);
+        } else{
+         screenValue +=butn;
+          screen.value = screenValue;
+        }
+      ep.preventDefault();
+    })
+}
+
+
+        
